05dcc92bb1b91a6562795a53d40ddb6bf5993c5a
pravakar15031986 <68266118+pravakar15031986@users.noreply.github.com>
2025-04-11 08:45:53 +0530

    Update app.py

--- code/app.py
+++ code/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '<h1>Hello World from app Flaskfor testing purpose some changes are happen</h2>'
+    return '<h1>Hello World from app Flaskfor testing purpose </h2>'
 
 if __name__ == "__main__":
     app.run(debug=True)
